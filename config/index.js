9875debc24d66c31180b2110df4dee2c906e196e
Manav <manav@ith.tech>
2023-04-28 11:21:03 +0530

    clientUrl added to config

--- config/index.js
+++ config/index.js
@@ -11,7 +11,6 @@ const commonVariables = {
     SERVICE_REST_PORT: '8004',
     SERVICE_RPC_PORT: '8005',
     DB_ENV: 'development',
-    CLIENT_URL:'http://localhost:3000',
     pageLimit: 10,
     REWARD_POT: ["REWARDPOT", "LOTTERYPOT"],
     ASSEST_TYPE: ["TOKEN", "NFT"],
